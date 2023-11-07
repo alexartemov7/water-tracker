b06d762a8c4bd9f1133b134e3247a2d565e7425b
Alex Artemov <artemov.sandro@gmail.com>
2023-11-07 15:43:50 -0500

    Volume goal is tracked

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import { GluestackUIProvider, Box } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
 import Title from "./components/Title";
+import Goal from "./components/Goal";
 import WaterImage from "./components/WaterImage";
 
 export default function App() {
+  const [volume, setVolume] = useState(500)
+
   return (
     <GluestackUIProvider config={config}>
       <Box w='100%' h="$full" justifyContent="center" alignItems="center" bgColor="$blue300">
         <Title />
+        <Goal volume={volume}/>
         <WaterImage />
       </Box>
     </GluestackUIProvider>
